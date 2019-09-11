9b2444d3c59d32ca35bee8afaf4eef4232ea0e2f
KrotAphanasiy <52762504+KrotAphanasiy@users.noreply.github.com>
2019-09-11 20:15:31 +0300

    Add files via upload

---
+++ Cavestory/source/headers/globals.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALS_H // some global variables
+#define GLOBALS_H
+
+namespace globals {
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+
+	const float SPRITE_SCALE = 2.0;
+}
+
+
+#endif
